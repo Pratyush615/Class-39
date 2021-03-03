3abb930a9b83e988cba378f3e7cedbbe4585107a
Pratyush615 <mathurpm@gmail.com>
2021-03-03 16:36:57 -0500

    Added car sprites and a gameState end fucntion

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage(car1Image);
     car2 = createSprite(300,200);
+    car2.addImage(car2Image);
     car3 = createSprite(500,200);
+    car3.addImage(car3Image);
     car4 = createSprite(700,200);
+    car4.addImage(car4Image);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background("#c68767");
+      image(trackImage,0,-displayHeight*4,displayWidth,displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -56,7 +61,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 250;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -76,9 +81,15 @@ class Game {
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=10
+      console.log(player.distance);
       player.update();
     }
-
+    if(player.distance> 4180){
+      gameState = 2;
+    }
     drawSprites();
+  }
+  end(){
+    console.log("game ended");
   }
 }
